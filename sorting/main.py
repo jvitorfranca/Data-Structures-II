d83a8ef06df2c6615865f8cc3ee79a144f27361e
jvitorfranca <vitoorbr@gmail.com>
2018-08-25 18:44:07 -0300

    creating random numbers

--- sorting/main.py
+++ sorting/main.py
@@ -8,14 +8,29 @@ def how_to_use():
     print(' Example: python3 main.py -a insert -q 1000 -m random ')
     print('------------------------------------------------------')
 
+#generating random numbers without repeats
+def create_random_integers(my_list, elements, method):
+
+    if method == 'random':
+        my_list = random.sample(range(elements), elements)
+
+    elif method == 'ascending':
+        for i in range(1, elements):
+            my_list.append(i)
+
+    elif method == 'descending':
+        for i in range(elements, 1, -1):
+            my_list.append(i)
+
+    return my_list
+
 def main(argv):
     #defining our initial time
     time.start = time.clock()
 
     list = []
 
-    #generating random numbers without repeats
-    list = random.sample(range(100), 15)
+    list = create_random_integers(list, 100, 'descending')
 
     #calculating the time used to process the algorithms
     time_elapsed = (time.clock() - time.start)
