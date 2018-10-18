c4f05bb88b38048fe59e1fe674de0d4c61d515f8
jvitorfranca <vitoorbr@gmail.com>
2018-10-18 13:52:35 -0300

    Menu prototype

--- hashing/main.py
+++ hashing/main.py
@@ -8,11 +8,17 @@ def main(argv):
 
 	words = ut.loadWords(argv[1:])
 
-	# tad = ut.loadRedBlackTree(words)
-	# tad.invertedIndex()
+	choice = int(input("1. RedBlack Tree \n2. Linear Probing\n"))
 
-	tad = ut.loadHashTable(words)
-	tad.invertedIndex(words)
+	if choice == 1:
+		tad = ut.loadRedBlackTree(words)
+		tad.invertedIndex()
+	elif choice == 2:
+		tad = ut.loadHashTable(words)
+		tad.invertedIndex(words)
+	else:
+		print("Invalid argument")
+		sys.exit(2)
 
 if __name__ == "__main__":
 	main(sys.argv)
