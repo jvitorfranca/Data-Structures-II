923f96475369fdb0e3cd2b0cf461acd902f1fa37
jvitorfranca <vitoorbr@gmail.com>
2018-10-26 14:11:14 -0300

    Adding memmory usage

--- hashing/main.py
+++ hashing/main.py
@@ -1,9 +1,10 @@
 # Inverted Index implementation
 # Author: João Vitor Ferreira França
 
+import os
+import psutil
 import sys
 import time
-import red_black_tree as rb
 import utilities as ut
 
 
@@ -17,12 +18,17 @@ def main(argv):
 	# Parameter to count the time spent
 	execution_time = 0
 
+	# Getting the memory informations
+	process = psutil.Process(os.getpid())
+
+	mem_initial = process.memory_info().rss / float(2 ** 20)
+
 	# Defining which ADT the program will run
 	if option == 1:
 
 		# Just a flag to know what ADT will be used
 		tad_type = "redblack"
-		start_time = time.time()
+		start_time = time.process_time()
 
 		# Loading the words from the initial files
 		words = ut.loadWords(argv[1:])
@@ -34,12 +40,18 @@ def main(argv):
 		tad.invertedIndex()
 
 		# Calculating the time spent
-		execution_time = time.time() - start_time
-		print("Execution time ", "%5f" % execution_time)
+		execution_time = time.process_time() - start_time
+
+		print("Execution time ", execution_time)
+
+		mem_final = process.memory_info().rss / float(2 ** 20)
+
+		print("Memory used: ", "%5f" % (mem_final - mem_initial))
+
 	elif option == 2:
 
 		tad_type = "hash"
-		start_time = time.time()
+		start_time = time.process_time()
 
 		# Loading the words from the initial files
 		words = ut.loadWords(argv[1:])
@@ -51,12 +63,17 @@ def main(argv):
 		tad.invertedIndex(words)
 
 		# Calculating the time spent
-		execution_time = time.time() - start_time
-		print("Execution time ", "%5f" % execution_time)
+		execution_time = time.process_time() - start_time
+		print("Execution time ", execution_time)
+
+		mem_final = process.memory_info().rss / float(2 ** 20)
+
+		print("Memory used: ", "%5f" % (mem_final - mem_initial))
+
 	elif option == 3:
 
 		tad_type = "hash"
-		start_time = time.time()
+		start_time = time.process_time()
 
 		# Loading the words from the initial files
 		words = ut.loadWords(argv[1:])
@@ -68,8 +85,13 @@ def main(argv):
 		tad.invertedIndex(words)
 
 		# Calculating the time spent
-		execution_time = time.time() - start_time
-		print("Execution time ", "%5f" % execution_time)
+		execution_time = time.process_time() - start_time
+		print("Execution time ", execution_time)
+
+		mem_final = process.memory_info().rss / float(2 ** 20)
+
+		print("Memory used: ", "%5f" % (mem_final - mem_initial))
+
 	else:
 		# If the user did not choose a valid ADT
 		print("Error, you must choose a valid input")
@@ -83,14 +105,18 @@ def main(argv):
 		if option == 2:
 			sys.exit()
 		elif option == 1:
-			start_time = time.time()
+			start_time = time.process_time()
 
 			# Calculating the Inverse Document Frequency
 			ut.IDF(argv[1:], tad, tad_type)
 			
 			# Calculating the time spent
-			execution_time = time.time() - start_time
-			print("Time to IDF: ", "%5f" % execution_time)
+			execution_time = time.process_time()
+			print("Time to IDF: ", execution_time, start_time)
+
+			mem_final = process.memory_info().rss / float(2 ** 20)
+
+			print("Memory used: ", "%5f" % (mem_final - mem_initial))
 
 if __name__ == "__main__":
 	main(sys.argv)
